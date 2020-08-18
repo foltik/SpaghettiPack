d0a2428c73c264a445dc6e4d077a589360f366ef
Jack Foltz <jack@foltz.io>
2020-08-18 17:08:21 -0400

    Update to 0.2.0

---
+++ utils/update.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const got = require('got');
+
+const file = process.argv[2];
+
+if (!file) {
+    console.log('Usage: update.js /path/to/info.json');
+    process.exit(1);
+}
+
+const config = JSON.parse(fs.readFileSync(file, 'utf8'));
+
+let mods = config.dependencies.map(d => {
+    const [name, version] = d.split(' >= ');
+    return {name, version};
+});
+
+(async () => {
+    console.log('Retrieving latest mod versions...');
+    console.log();
+
+    await Promise.all(mods.filter(m => m.name != 'base').map(async m => {
+        try {
+            const r = await got(`https://mods.factorio.com/api/mods/${m.name}`);
+            const body = JSON.parse(r.body);
+
+            const releases = body.releases;
+            const latest = releases[releases.length - 1].version;
+
+            m.latest = latest;
+        } catch (e) {
+            console.log(`Error retrieving versions for ${m.name}:`);
+            console.log(e);
+        }
+    }));
+
+    const updated = mods.filter(m => m.latest && m.latest != m.version);
+
+    if (updated.length) {
+        console.log(`Found ${updated.length} mods with new versions:`);
+        updated.forEach(m => console.log(`${m.name}: ${m.version} -> ${m.latest}`));
+        config.dependencies = mods.map(m => `${m.name} >= ${m.latest || m.version}`);
+        console.log();
+
+        console.log('Writing new versions to file...');
+        console.log();
+        fs.writeFileSync(file, JSON.stringify(config, null, 4));
+
+        console.log('Done!');
+    } else {
+        console.log('All mods are up to date!');
+    }
+})();
